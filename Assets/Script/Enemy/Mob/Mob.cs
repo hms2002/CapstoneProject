89a1be6b942fe97b81346b70fdfeb8695522a744
Panabolic <4097lsh@naver.com>
2026-02-05 16:50:02 +0900

    흠

--- Assets/Script/Enemy/Mob/Mob.cs
+++ Assets/Script/Enemy/Mob/Mob.cs
@@ -13,7 +13,7 @@ public class Mob : Enemy
     [SerializeField] private AbilityDefinition tackleAbility; // AD_Tackle
 
     // Variables
-    private float   attackCoolTime; // 마지막 공격 시간을 저장하여 쿨타임 체크용으로 사용
+    private float   currentCooltime; // 마지막 공격 시간을 저장하여 쿨타임 체크용으로 사용
     private bool    isDead = false;
 
     // ========================================================================
@@ -23,24 +23,34 @@ public class Mob : Enemy
     {
         if (isDead || target == null) return;
 
+        // 쿨타임 처리 (카운트다운)
+        if (currentCooltime > 0)
+        {
+            currentCooltime -= Time.deltaTime;
+            if (currentCooltime < 0) currentCooltime = 0;
+        }
+
         Vector2 finalVelocity   = Vector2.zero;
                 targetDistance  = Vector2.Distance(transform.position, target.position);
 
         // 타겟 탐지
-        if (targetDistance <= detectionRange)
+        if (currentCooltime <= 0)
         {
-            moveDirection = (target.position - transform.position).normalized;
-            finalVelocity = moveDirection * moveSpeed;
+            if (targetDistance <= detectionRange)
+            {
+                moveDirection = (target.position - transform.position).normalized;
+                finalVelocity = moveDirection * moveSpeed;
 
-            // 스프라이트 반전
-            if      (transform.position.x > target.position.x) sprite.flipX = true;
-            else if (transform.position.x < target.position.x) sprite.flipX = false;
-        }
+                // 스프라이트 반전
+                if      (transform.position.x > target.position.x) sprite.flipX = true;
+                else if (transform.position.x < target.position.x) sprite.flipX = false;
+            }
 
-        // [넉백 예외처리]: 외부 충격(넉백)으로 인해 속도가 엄청 빠르면 내 의지로 덮어쓰지 않음
-        if (rigid2D.linearVelocity.magnitude <= moveSpeed * 1.5f)
-        {
-            rigid2D.linearVelocity = finalVelocity; 
+            // 넉백 예외처리 - 외부 충격(넉백)으로 인해 속도가 엄청 빠르면 내 의지로 덮어쓰지 않음
+            if (rigid2D.linearVelocity.magnitude <= moveSpeed * 1.5f)
+            {
+                rigid2D.linearVelocity = finalVelocity;
+            }
         }
 
         animator.SetBool("isMoving", finalVelocity.sqrMagnitude > 0.01f);
@@ -55,23 +65,14 @@ public class Mob : Enemy
 
         if (collision.gameObject.CompareTag("Player"))
         {
-            // Mob.cs 레벨에서의 쿨타임 체크 (불필요한 GAS 호출 방지)
-            if (Time.time >= attackCoolTime + damageInterval)
+            // 쿨타임이 0일 때만 공격 시도
+            if (currentCooltime <= 0)
             {
-                RequestAttack(collision.gameObject);
-                attackCoolTime = Time.time;
-            }
-        }
-    }
+                abilitySystem.TryActivateAbility(tackleAbility, target.gameObject);
 
-    private void RequestAttack(GameObject targetObj)
-    {
-        // 1. 할당된 어빌리티(AD)가 있는지 확인
-        if (tackleAbility != null)
-        {
-            // 2. GAS 시스템에 시전 요청
-            // Mob은 "어떻게 때리는지" 모름. 그냥 AD_Tackle을 실행하라고 시킬 뿐.
-            abilitySystem.TryActivateAbility(tackleAbility, targetObj);
+                // 공격 성공 시 쿨타임(경직) 시작
+                currentCooltime = damageInterval;
+            }
         }
     }
 
