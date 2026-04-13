87caa6124bdcdd58b5f6b1b53cfedce849273b09
nadoman354 <nadoman354@naver.com>
2026-04-14 01:43:30 +0900

    4/14
    보스 페이즈 전환 패턴 아직 폴리싱 진행중(작동은 함)

--- Assets/Script/Enemy/Boss/FSM/BossControllers/WitchBoss/Abilities/AbilityLogic_WitchLightAllCandles.cs
+++ Assets/Script/Enemy/Boss/FSM/BossControllers/WitchBoss/Abilities/AbilityLogic_WitchLightAllCandles.cs
@@ -1,16 +1,39 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityGAS;
 
 public class AbilityLogic_WitchLightAllCandles : AbilityLogic
 {
-    // 이 클래스의 책임:
-    // 페이즈 전환 패턴 동안 중앙 이동, 맵 전역 경고, 전 촛대 봉인, 제한 시간 종료 피해를 순서대로 처리한다.
-
     private const float MoveToCenterDuration = 0.45f;
     private const float RelightDeadlineSeconds = 8f;
     private const float ShieldBreakWaitGraceSeconds = 1.5f;
+    private const float DefaultPresentationLifetimeSeconds = 1f;
+
+    [Header("Charge Orb")]
+    [SerializeField] private GameObject chargeOrbPrefab;
+    [SerializeField] private Vector3 chargeOrbLocalOffset = new Vector3(0f, 1.35f, -0.05f);
+    [SerializeField] private Vector3 chargeOrbStartScale = new Vector3(0.25f, 0.25f, 1f);
+    [SerializeField] private Vector3 chargeOrbEndScale = new Vector3(1.35f, 1.35f, 1f);
+    [SerializeField] private bool followWitchDuringCharge = true;
+    [SerializeField] [Min(0.01f)] private float chargeOrbFailureDropDuration = 0.22f;
+    [SerializeField] private Vector3 chargeOrbImpactLocalOffset = new Vector3(0f, 0.1f, -0.05f);
+
+    [Header("Charge Presentation")]
+    [SerializeField] private CameraShakeHook chargeLoopCameraShake = CameraShakeHook.Create(0.03f, 1f, 0.08f, 0.08f);
+
+    [Header("Failure Presentation")]
+    [SerializeField] private GameObject failureEffectPrefab;
+    [SerializeField] private Vector3 failureEffectLocalOffset = new Vector3(0f, 0f, -0.05f);
+    [SerializeField] [Min(0f)] private float failureEffectLifetimeSeconds = 0.35f;
+    [SerializeField] private Vector3 failureEffectScaleMultiplier = Vector3.one;
+    [SerializeField] private float failureEffectRotationOffsetZ;
+    [SerializeField] private GameObject failureParticlePrefab;
+    [SerializeField] private Vector3 failureParticleLocalOffset = new Vector3(0f, 0f, -0.02f);
+    [SerializeField] [Min(0f)] private float failureParticleLifetimeOverrideSeconds = 0f;
+    [SerializeField] private bool useUnscaledFailureParticleTime;
+    [SerializeField] private Vector3 failureParticleScaleMultiplier = Vector3.one;
+    [SerializeField] private float failureParticleRotationOffsetZ;
+    [SerializeField] private CameraShakeHook failureCameraShake = CameraShakeHook.Create(0.24f, 1f, 0.36f, 0.05f);
 
     public override IEnumerator Activate(AbilitySystem system, AbilitySpec spec, GameObject initialTarget)
     {
@@ -29,11 +52,21 @@ public class AbilityLogic_WitchLightAllCandles : AbilityLogic
         witch.SpeakSituation(BossSpeechSituationEnum.UltimateWarning);
         witch.ShowMapWideWarning(center, RelightDeadlineSeconds);
         witch.SealAllCandles();
-        float deadlineTime = Time.time + RelightDeadlineSeconds;
+
+        GameObject chargeOrbInstance = SpawnChargeOrb(witch);
+        float chargeStartTime = Time.time;
+        float deadlineTime = chargeStartTime + RelightDeadlineSeconds;
         bool allCandlesRelit = false;
 
         while (Time.time < deadlineTime)
         {
+            float chargeProgress = Mathf.InverseLerp(chargeStartTime, deadlineTime, Time.time);
+            UpdateChargeOrb(witch, chargeOrbInstance, chargeProgress);
+            chargeLoopCameraShake.TryPlay(
+                source: witch.gameObject,
+                fallbackDirection: Vector3.up,
+                debugReason: "WitchLightAllCandles.Charging");
+
             if (!witch.HasAnySealedCandles())
             {
                 allCandlesRelit = true;
@@ -45,6 +78,8 @@ public class AbilityLogic_WitchLightAllCandles : AbilityLogic
 
         if (allCandlesRelit)
         {
+            CleanupChargeOrb(chargeOrbInstance);
+
             float shieldBreakDeadline = Time.time + ShieldBreakWaitGraceSeconds;
 
             while (witch.ShieldController != null && witch.ShieldController.HasShield && Time.time < shieldBreakDeadline)
@@ -60,9 +95,259 @@ public class AbilityLogic_WitchLightAllCandles : AbilityLogic
 
         if (witch.HasAnySealedCandles())
         {
+            yield return PlayFailurePresentation(witch, chargeOrbInstance);
             witch.DisableStaggerImmuneDuringPhaseTransition();
             witch.ClearShield();
             witch.ApplyMapWideDamage(initialTarget);
+            yield break;
         }
+
+        CleanupChargeOrb(chargeOrbInstance);
+    }
+
+    private GameObject SpawnChargeOrb(Witch witch)
+    {
+        if (witch == null || chargeOrbPrefab == null)
+            return null;
+
+        Vector3 spawnPosition = ResolveChargeOrbPosition(witch);
+        GameObject instance = Object.Instantiate(chargeOrbPrefab, spawnPosition, chargeOrbPrefab.transform.rotation);
+        if (instance == null)
+            return null;
+
+        instance.transform.localScale = chargeOrbStartScale;
+        ConfigureSpawnedPresentation(instance, useUnscaledTime: false);
+        return instance;
+    }
+
+    private void UpdateChargeOrb(Witch witch, GameObject chargeOrbInstance, float chargeProgress)
+    {
+        if (witch == null || chargeOrbInstance == null)
+            return;
+
+        if (followWitchDuringCharge)
+            chargeOrbInstance.transform.position = ResolveChargeOrbPosition(witch);
+
+        chargeOrbInstance.transform.localScale = Vector3.LerpUnclamped(
+            chargeOrbStartScale,
+            chargeOrbEndScale,
+            Mathf.Clamp01(chargeProgress));
+    }
+
+    private IEnumerator PlayFailurePresentation(Witch witch, GameObject chargeOrbInstance)
+    {
+        Vector3 impactPosition = ResolveChargeOrbImpactPosition(witch);
+
+        if (chargeOrbInstance != null)
+        {
+            Vector3 startPosition = chargeOrbInstance.transform.position;
+            Vector3 startScale = chargeOrbInstance.transform.localScale;
+            float elapsed = 0f;
+
+            while (elapsed < chargeOrbFailureDropDuration)
+            {
+                elapsed += Time.deltaTime;
+                float t = Mathf.Clamp01(elapsed / chargeOrbFailureDropDuration);
+                float eased = 1f - Mathf.Pow(1f - t, 3f);
+
+                chargeOrbInstance.transform.position = Vector3.Lerp(startPosition, impactPosition, eased);
+                chargeOrbInstance.transform.localScale = Vector3.LerpUnclamped(startScale, chargeOrbEndScale, eased);
+                yield return null;
+            }
+        }
+
+        CleanupChargeOrb(chargeOrbInstance);
+
+        SpawnPresentationPrefab(
+            failureEffectPrefab,
+            impactPosition + failureEffectLocalOffset,
+            failureEffectRotationOffsetZ,
+            failureEffectScaleMultiplier,
+            failureEffectLifetimeSeconds,
+            useUnscaledTime: false);
+        SpawnPresentationPrefab(
+            failureParticlePrefab,
+            impactPosition + failureParticleLocalOffset,
+            failureParticleRotationOffsetZ,
+            failureParticleScaleMultiplier,
+            failureParticleLifetimeOverrideSeconds,
+            useUnscaledFailureParticleTime);
+
+        failureCameraShake.TryPlay(
+            source: witch != null ? witch.gameObject : null,
+            fallbackDirection: Vector3.down,
+            debugReason: "WitchLightAllCandles.FailImpact");
+    }
+
+    private Vector3 ResolveChargeOrbPosition(Witch witch)
+    {
+        return witch != null
+            ? witch.transform.TransformPoint(chargeOrbLocalOffset)
+            : chargeOrbLocalOffset;
+    }
+
+    private Vector3 ResolveChargeOrbImpactPosition(Witch witch)
+    {
+        return witch != null
+            ? witch.transform.TransformPoint(chargeOrbImpactLocalOffset)
+            : chargeOrbImpactLocalOffset;
+    }
+
+    private static void CleanupChargeOrb(GameObject chargeOrbInstance)
+    {
+        if (chargeOrbInstance != null)
+            Object.Destroy(chargeOrbInstance);
+    }
+
+    private static void SpawnPresentationPrefab(
+        GameObject prefab,
+        Vector3 position,
+        float rotationOffsetZ,
+        Vector3 scaleMultiplier,
+        float lifetimeOverrideSeconds,
+        bool useUnscaledTime)
+    {
+        if (prefab == null)
+            return;
+
+        Quaternion rotation = Quaternion.Euler(0f, 0f, rotationOffsetZ);
+        GameObject instance = Object.Instantiate(prefab, position, rotation);
+        if (instance == null)
+            return;
+
+        instance.transform.localScale = Vector3.Scale(instance.transform.localScale, scaleMultiplier);
+        ConfigureSpawnedPresentation(instance, useUnscaledTime);
+
+        float lifetime = ResolvePresentationLifetime(instance, lifetimeOverrideSeconds);
+        if (lifetime > 0f)
+            Object.Destroy(instance, lifetime);
+    }
+
+    private static void ConfigureSpawnedPresentation(GameObject instance, bool useUnscaledTime)
+    {
+        if (instance == null)
+            return;
+
+        instance.SetActive(true);
+
+        ParticleSystem[] particleSystems = instance.GetComponentsInChildren<ParticleSystem>(includeInactive: true);
+        for (int i = 0; i < particleSystems.Length; i++)
+        {
+            ParticleSystem particleSystem = particleSystems[i];
+            if (particleSystem == null)
+                continue;
+
+            if (useUnscaledTime)
+            {
+                ParticleSystem.MainModule main = particleSystem.main;
+                main.useUnscaledTime = true;
+            }
+
+            particleSystem.Play(withChildren: true);
+        }
+
+        Animation[] animations = instance.GetComponentsInChildren<Animation>(includeInactive: true);
+        for (int i = 0; i < animations.Length; i++)
+        {
+            Animation animationComponent = animations[i];
+            if (animationComponent == null)
+                continue;
+
+            animationComponent.Play();
+        }
+    }
+
+    private static float ResolvePresentationLifetime(GameObject instance, float lifetimeOverrideSeconds)
+    {
+        if (lifetimeOverrideSeconds > 0f)
+            return lifetimeOverrideSeconds;
+
+        float particleLifetime = ResolveParticleLifetime(instance);
+        if (particleLifetime > 0f)
+            return particleLifetime;
+
+        float animationLifetime = ResolveAnimatorLifetime(instance);
+        if (animationLifetime > 0f)
+            return animationLifetime;
+
+        return DefaultPresentationLifetimeSeconds;
+    }
+
+    private static float ResolveParticleLifetime(GameObject instance)
+    {
+        ParticleSystem[] particleSystems = instance.GetComponentsInChildren<ParticleSystem>(includeInactive: true);
+        if (particleSystems == null || particleSystems.Length == 0)
+            return 0f;
+
+        float maxLifetime = 0f;
+        for (int i = 0; i < particleSystems.Length; i++)
+        {
+            ParticleSystem particleSystem = particleSystems[i];
+            if (particleSystem == null)
+                continue;
+
+            ParticleSystem.MainModule main = particleSystem.main;
+            if (main.loop)
+                return DefaultPresentationLifetimeSeconds;
+
+            float startDelay = ResolveCurveMax(main.startDelay);
+            float startLifetime = ResolveCurveMax(main.startLifetime);
+            maxLifetime = Mathf.Max(maxLifetime, startDelay + main.duration + startLifetime);
+        }
+
+        return maxLifetime > 0f ? maxLifetime + 0.25f : 0f;
+    }
+
+    private static float ResolveAnimatorLifetime(GameObject instance)
+    {
+        float maxLifetime = 0f;
+
+        Animator[] animators = instance.GetComponentsInChildren<Animator>(includeInactive: true);
+        for (int i = 0; i < animators.Length; i++)
+        {
+            Animator animator = animators[i];
+            if (animator == null || animator.runtimeAnimatorController == null)
+                continue;
+
+            AnimationClip[] clips = animator.runtimeAnimatorController.animationClips;
+            for (int clipIndex = 0; clipIndex < clips.Length; clipIndex++)
+            {
+                AnimationClip clip = clips[clipIndex];
+                if (clip == null)
+                    continue;
+
+                maxLifetime = Mathf.Max(maxLifetime, clip.length);
+            }
+        }
+
+        Animation[] animations = instance.GetComponentsInChildren<Animation>(includeInactive: true);
+        for (int i = 0; i < animations.Length; i++)
+        {
+            Animation animationComponent = animations[i];
+            if (animationComponent == null)
+                continue;
+
+            foreach (AnimationState state in animationComponent)
+            {
+                if (state?.clip == null)
+                    continue;
+
+                maxLifetime = Mathf.Max(maxLifetime, state.clip.length);
+            }
+        }
+
+        return maxLifetime > 0f ? maxLifetime + 0.05f : 0f;
+    }
+
+    private static float ResolveCurveMax(ParticleSystem.MinMaxCurve curve)
+    {
+        return curve.mode switch
+        {
+            ParticleSystemCurveMode.Constant => curve.constant,
+            ParticleSystemCurveMode.TwoConstants => curve.constantMax,
+            ParticleSystemCurveMode.Curve => curve.curveMultiplier,
+            ParticleSystemCurveMode.TwoCurves => curve.curveMultiplier,
+            _ => Mathf.Max(curve.constant, curve.constantMax)
+        };
     }
 }
